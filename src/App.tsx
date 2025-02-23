40fe1dcad3d066e8bc8b3ba12103ff0eb803fdac
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-02-23 15:02:55 -0300

    feat: Header e Personal Card implementados

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import Header from "./components/Header/Header";
+import PersonalCard from "./components/PersonalCard/PersonalCard";
+
 export default function App() {
   return (
-    <></>
+    <>
+      <Header/>
+      <PersonalCard/>
+    </>
   )
 }
 
