23013aaca910e68572f5d897c1fa05c6f6b4b79e
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-02-23 20:48:11 -0300

    feat: componente do botão de comentário da postagem criado

---
+++ src/components/CommentButton/CommentButton.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css'
+
+export default function CommentButton() {
+    return (
+        <div className={styles.button}>
+            <button>Comentar</button>
+        </div>
+    )
+}
