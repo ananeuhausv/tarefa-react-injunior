9c1f10ace0c4393abaea0083926c94815338c619
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-02-24 01:12:01 -0300

    feat: timePosted agora é dinâmico (de acordo com o tempo de abertura da página)

--- src/components/CardPost/cardPost.tsx
+++ src/components/CardPost/cardPost.tsx
@@ -1,25 +1,44 @@
-import CommentButton from '../CommentButton/CommentButton'
+import { useEffect, useState } from 'react'
 import HeaderCardPost from '../HeaderCardPost/HeaderCardPost'
 import TextareaCardPost from '../TextareaCardPost/TextareaCardPost'
 import styles from './styles.module.css'
+import Comments from '../Comments/Comments'
+
 
 export interface CardPostProps{
     picture: string
     name: string
     profession: string
-    timePosted: string
     post?: string
+    timePosted: Date
 }
 
-export default function CardPost({ picture, name, profession, timePosted, post }: CardPostProps) {
+export default function CardPost({ picture, name, profession, post }: CardPostProps) {
+    const [timePosted, setTimePosted] = useState<Date | null>(null);
+
+    useEffect(() => {
+        setTimePosted(new Date());
+    },[]);
+
+    const [comments, setComments] = useState<string[]>([]);
+
+    const handleAddComment = (comment: string) => {
+        setComments([...comments, comment]);
+    };
+
     return (
         <div className={styles.container}>
-        <HeaderCardPost name={name} picture={picture} profession={profession} timePosted={timePosted}/>
+            {timePosted && (
+                <HeaderCardPost name={name} picture={picture} profession={profession} timePosted={timePosted}/>
+            )}
         <div className={styles.post}>
             <p>{post}</p>
         </div>
-        <TextareaCardPost/>
-        <CommentButton/>
+        <TextareaCardPost onAddComment={handleAddComment}/>
+        {comments.map((comment, index)=> (
+            <Comments key={index} comment={ comment }/>
+        )) }
+        
         </div>
     )
 }
