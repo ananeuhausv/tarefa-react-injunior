bac5321877d87bdbd8630f7d1de487bc433afb55
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-02-24 01:07:20 -0300

    feat: ao clicar no botão 'Comentar' o comentário aparecerá na tela

--- src/components/TextareaCardPost/TextareaCardPost.tsx
+++ src/components/TextareaCardPost/TextareaCardPost.tsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import styles from './styles.module.css'
+import CommentButton from '../CommentButton/CommentButton';
 
-export default function TextareaCardPost() {
+interface TextareaCardPostProps {
+    onAddComment: (comment: string) => void;
+}
+
+export default function TextareaCardPost({ onAddComment }: TextareaCardPostProps) {
+    const[text, setText] = useState("");
+
+    const handleSubmit = () => {
+        if(text.trim() === "") return
+        onAddComment(text);
+        setText("");
+    }
     return (
         <div className={styles.feedback}>
             <h3>Deixe seu feedback</h3>
-            <textarea placeholder="Escreva um comentário..."></textarea>
+            <textarea 
+            placeholder="Escreva um comentário..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}></textarea>
+            <CommentButton onClick={handleSubmit}/>
         </div> 
     )
 }
