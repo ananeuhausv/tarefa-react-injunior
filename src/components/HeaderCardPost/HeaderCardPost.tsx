719ef8c2811f05462207990534a990761cc07f5c
Ana Laura Neuhaus Vega <ananeuhaus@id.uff.br>
2025-02-24 01:04:44 -0300

    feat: mudança de tempo fixo para tempo dinâmico (de acordo com a abertura da paǵina)

--- src/components/HeaderCardPost/HeaderCardPost.tsx
+++ src/components/HeaderCardPost/HeaderCardPost.tsx
@@ -1,7 +1,34 @@
 import styles from './styles.module.css'
 import { CardPostProps } from '../CardPost/cardPost'
+import { useEffect, useRef, useState } from 'react';
+
+const formatTime = (time: Date) => {
+    const postDate = new Date(time);
+    const now = new Date();
+    const diffInSeconds = Math.floor((now.getTime() - postDate.getTime()) / 1000);
+
+    if (diffInSeconds < 60) return `Postado há ${diffInSeconds} segundos`;
+    const diffInMinutes = Math.floor(diffInSeconds / 60);
+    if (diffInMinutes < 60) return `Postado há ${diffInMinutes} minutos`;
+    const diffInHours = Math.floor(diffInMinutes / 60);
+    if (diffInHours < 24) return `Postado há ${diffInHours} horas`;
+    const diffInDays = Math.floor(diffInHours / 24);
+    return `Postado há ${diffInDays} dias`;
+};
 
 export default function HeaderCardPost({ picture, name, profession, timePosted }: CardPostProps) {
+    const[formattedTime, setFormattedTime] = useState(formatTime(timePosted));
+    const timePostedRef = useRef(timePosted);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setFormattedTime(formatTime(timePostedRef.current));
+        },10000);
+
+        return () => clearInterval(interval);
+    },[]);
+    
+
     return (
         <div className={styles.header}>
         <div>
@@ -11,7 +38,7 @@ export default function HeaderCardPost({ picture, name, profession, timePosted }
                 <p>{profession}</p>
             </div>
         </div> 
-    <p>{timePosted}</p>
+        <p>{formattedTime}</p>
     </div> 
     )
 }
